016b31a62da465d914591eeda6f71a7c3e4c5048
semianchuk <fix20152@gmail.com>
2016-10-16 13:19:54 +0300

    poster

---
+++ src/controller/mainController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by semianchuk on 08.10.16.
+ */
+angular.module('angularApp')
+    .controller('mainController', [ '$scope', 'mainFactory', 'mainService', 'mainProvider', function ($scope, mainFactory, mainService, mainProvider) {
+        
+        $scope.getFactory  = mainFactory.getPrivate();
+        $scope.getService  = mainService.getPrivate();
+        $scope.getProvider = mainProvider.getPrivate();
+        var f = () => alert('ok');
+        f()
+
+    }]);
